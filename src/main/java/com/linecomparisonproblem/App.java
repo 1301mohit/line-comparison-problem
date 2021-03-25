c4bc6c0a2e9b9b8b4bf9c7ece8bdae715c1b71d0
mohit <mohitkr1301>
2021-03-25 12:38:39 +0530

    Call calculatelength method of Lengh class and print length of line

--- src/main/java/com/linecomparisonproblem/App.java
+++ src/main/java/com/linecomparisonproblem/App.java
@@ -1,13 +1,12 @@
 package com.linecomparisonproblem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Line Comparison Problem" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Problem");
+		Length length = new Length(2.0, 3.0, 4.0, 5.0);
+		double lengthOfLine = length.calculateLength();
+		System.out.printf("Length of line is %.2f", lengthOfLine);
+	}
+	
 }

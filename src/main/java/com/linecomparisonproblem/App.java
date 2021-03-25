2b81f538a5fc46f8e0d561cd34207a68ce0594fb
mohit <mohitkr1301>
2021-03-25 13:04:48 +0530

    Modified: Call equals method of length class

--- src/main/java/com/linecomparisonproblem/App.java
+++ src/main/java/com/linecomparisonproblem/App.java
@@ -4,9 +4,14 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Problem");
-		Length length = new Length(2.0, 3.0, 4.0, 5.0);
-		double lengthOfLine = length.calculateLength();
-		System.out.printf("Length of line is %.2f", lengthOfLine);
+		Length length1 = new Length(2.0, 3.0, 4.0, 5.0);
+		Length length2 = new Length(2.0, 3.0, 4.0, 5.0);
+		double lengthOfLine = length1.calculateLength();
+		System.out.printf("Length of line is %.2f\n", lengthOfLine);
+		if(length1.equals(length2)) 
+			System.out.println("Both lengths are equal");
+		else
+			System.out.println("Both lengths are not equal");
 	}
 	
 }

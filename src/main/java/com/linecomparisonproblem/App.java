1e3cb73027d79ac69564dd3d40a784ea2513c1d6
mohit <mohitkr1301>
2021-03-25 14:33:41 +0530

    Modified: Remove hard code

--- src/main/java/com/linecomparisonproblem/App.java
+++ src/main/java/com/linecomparisonproblem/App.java
@@ -3,25 +3,28 @@ package com.linecomparisonproblem;
 public class App {
 
 	public static void main(String[] args) {
+		final int IS_EQUAL = 0;
+		final int IS_GREATER = 1;
+
 		System.out.println("Welcome to Line Comparison Problem");
 		Line line1 = new Line(2.0, 3.0, 4.0, 7.0);
 		Line line2 = new Line(2.0, 3.0, 4.0, 6.0);
 		double lengthOfLine = line1.calculateLength();
 		System.out.printf("Length of line is %.2f\n", lengthOfLine);
-		if(line1.equals(line2)) 
+		if (line1.equals(line2))
 			System.out.println("Both lengths are equal");
 		else
 			System.out.println("Both lengths are not equal");
-		switch(line1.compareTo(line2)) {
-			case 0 :
-				System.out.println("Line1 and Line2 both are same");
-				break;
-			case 1 :
-				System.out.println("Line1 is greater than Line2");
-				break;
-			default :
-				System.out.println("Line 2 is greater than Line1");
+		switch (line1.compareTo(line2)) {
+		case IS_EQUAL:
+			System.out.println("Line1 and Line2 both are same");
+			break;
+		case IS_GREATER:
+			System.out.println("Line1 is greater than Line2");
+			break;
+		default:
+			System.out.println("Line 2 is greater than Line1");
 		}
 	}
-	
+
 }
